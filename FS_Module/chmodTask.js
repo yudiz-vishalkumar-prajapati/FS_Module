c2d25d1230470ffa208c0364306e1c68b4f9f0e9
Vishalkumar-Prajapati <131126614+Vishalkumar-Prajapati@users.noreply.github.com>
2023-04-19 18:24:17 +0530

    Modify the file

--- FS_Module/chmodTask.js
+++ FS_Module/chmodTask.js
@@ -1,6 +1,6 @@
 const fs=require('fs');
 
-fs.chmod('demo1.js',0o222,()=>{
+fs.chmod('demo1.js',0o444,()=>{
 try {
     fs.writeFileSync('demo1.js',"//change the text in file!!!!!!");
     const data=fs.readFileSync('demo1.js','utf-8');
